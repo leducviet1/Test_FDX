5bb82571f9c004c01a28b640d465b0fca4d0a21e
LeDucViet <ldviettb04@gmail.com>
2026-05-18 15:29:18 +0700

    Dashboard cơ bả

--- ThuVienSach/src/main/java/com/example/librarymanage_be/repo/FineRepository.java
+++ ThuVienSach/src/main/java/com/example/librarymanage_be/repo/FineRepository.java
@@ -2,7 +2,16 @@ package com.example.librarymanage_be.repo;
 
 import com.example.librarymanage_be.entity.Fine;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
+import java.math.BigDecimal;
+
 public interface FineRepository extends JpaRepository<Fine, Integer>, PagingAndSortingRepository<Fine, Integer> {
+    @Query("""
+       select coalesce(SUM(f.amount), 0)
+       FROM Fine f
+       where f.status = 'PAIDED'
+""")
+    BigDecimal sumPaidFine();
 }
