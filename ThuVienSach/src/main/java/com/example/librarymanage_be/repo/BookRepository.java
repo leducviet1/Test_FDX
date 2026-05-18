e4c0d9b0eac7a7e6dd4d83b72cae7be0bec3665a
LeDucViet <ldviettb04@gmail.com>
2026-05-18 15:20:04 +0700

    Chuyển Delete -> Soft Delete

--- ThuVienSach/src/main/java/com/example/librarymanage_be/repo/BookRepository.java
+++ ThuVienSach/src/main/java/com/example/librarymanage_be/repo/BookRepository.java
@@ -1,12 +1,27 @@
 package com.example.librarymanage_be.repo;
 
 import com.example.librarymanage_be.entity.Book;
+import com.example.librarymanage_be.enums.BookStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 public interface BookRepository extends
         JpaRepository<Book, Integer>,
         PagingAndSortingRepository<Book, Integer>,
         JpaSpecificationExecutor<Book> {
+    Page<Book> findAllByBookStatusNot(BookStatus bookStatus, Pageable pageable);
+
+    @Query("""
+        SELECT coalesce(sum(b.availableQuantity),0)
+        FROM Book b
+        WHERE b.bookStatus="ACTIVE"
+""")
+    long countAvailableBooks();
+
+
+//    void increaseQuantity();
 }
